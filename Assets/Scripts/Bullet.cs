2b6800acf3e47bcb23238f158708af45a786345d
James Deeman <james.deeman@gmail.com>
2014-10-04 21:55:21 +0100

    Initial commit.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    float speed;
+
+    bool alive;
+
+	// Use this for initialization
+	void Start ()
+    {
+        alive = true;
+        speed = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (alive)
+        {
+            transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
+
+            //Kill if out of bounds.
+            if (transform.position.y > 1000)
+            {
+                alive = false;
+            }
+        }
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+    private float speed;
+    private float verticalMovement;
+    private float horizontalMovement;
+
+    private GameObject shotPrefab;
+    private Bullet[] bullets;
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 5.0f;
+
+        shotPrefab = (GameObject)Resources.Load("shotPrefab");
+
+        /*
+        bullets = new Bullet[20];
+        for (int bulletID = 0; bulletID < bullets.Length; bulletID++)
+        {
+            bullets[bulletID] = new Bullet();
+        }
+         */
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        verticalMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        horizontalMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(horizontalMovement, verticalMovement, 0.0f);
+
+        if ( Input.GetButtonDown("Fire1") )
+        {
+            print("FIRE!");
+            Instantiate(shotPrefab, transform.position, transform.rotation);
+        }
+	}
+
+}
