b5b8fc46028f746e68f04d780daba43313d2b6c0
James Deeman <james.deeman@gmail.com>
2014-10-11 21:42:27 +0100

    Adding some assets for an enemy shot.

---
+++ Assets/Scripts/EnemyShot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShot : MonoBehaviour
+{
+		private Vector3 direction;
+		Vector3 Direction
+		{
+			set
+			{
+				direction = value.normalized;
+			}
+		}
+
+		private float speed;
+
+		// Use this for initialization
+		void Start ()
+		{
+				speed = 10.0f;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				transform.Translate (direction * speed);
+		}
+}
