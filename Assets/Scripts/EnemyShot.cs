78e874ec1b2b0fe601ec46704fc029051c7edcbf
James Deeman <james.deeman@gmail.com>
2014-10-11 23:11:05 +0100

    Enemy shot will now move in the direction it has. It will also destroy when it collides with something.

--- Assets/Scripts/EnemyShot.cs
+++ Assets/Scripts/EnemyShot.cs
@@ -3,26 +3,37 @@ using System.Collections;
 
 public class EnemyShot : MonoBehaviour
 {
-		private Vector3 direction;
-		public Vector3 Direction
+	private Vector3 direction;
+	public Vector3 Direction
+	{
+		set
 		{
-			set
-			{
-				direction = value.normalized;
-			}
+			direction = value.normalized;
+		}
+	}
+
+	private float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+			speed = 5.0f;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+			transform.Translate (direction * (speed * Time.deltaTime));
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+		{
+			return;
 		}
 
-		private float speed;
-
-		// Use this for initialization
-		void Start ()
-		{
-				speed = 5.0f;
-		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
-				transform.Translate (direction * (speed * Time.deltaTime));
-		}
+		coll.gameObject.SetActive(false);
+		Destroy(gameObject);
+	}
 }
