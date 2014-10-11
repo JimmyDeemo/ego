4513820fc24b59bb3c17de90b768d9534bb81ec9
James Deeman <james.deeman@gmail.com>
2014-10-11 20:47:54 +0100

    Class now uses the get and set 'Active' function to mark if it is available. Will kill after just 9 points on the y access.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,29 +5,31 @@ public class Bullet : MonoBehaviour
 {
     float speed;
 
-    bool alive;
-
 	// Use this for initialization
 	void Start ()
     {
-        alive = true;
         speed = 10.0f;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (alive)
+        if (gameObject.activeSelf)
         {
             transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
 
             //Kill if out of bounds.
-            if (transform.position.y > 1000)
+            if (transform.position.y > 9)
             {
-                alive = false;
+                gameObject.SetActive(false);
             }
         }
-
-
 	}
+
+    public void Reset(Vector3 startPosition)
+    {
+        transform.position = startPosition;
+        gameObject.SetActive(true);
+    }
 }
