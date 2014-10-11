ba2351e67bdfc7ba1ea2ed48ba423987f5b1bb5f
James Deeman <james.deeman@gmail.com>
2014-10-11 21:44:11 +0100

    Adding a game controller class to spawn some enemies.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject enemyShot;
+	public Vector3 spawnPostion;
+
+	private float nextSpawnTime;
+	private float spawnFrequency; //In seconds.
+
+	// Use this for initialization
+	void Start ()
+	{
+		nextSpawnTime = Time.time;
+		spawnFrequency = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		SpawnEnemies();
+	}
+
+	void SpawnEnemies()
+	{
+		if (Time.time > nextSpawnTime)
+		{
+			nextSpawnTime = Time.time + spawnFrequency;
+		}
+	}
+}
