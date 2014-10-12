3924b68c3f8cae4e82e92c1828959cb1609a299b
James Deeman <james.deeman@gmail.com>
2014-10-12 20:31:31 +0100

    Enemy bullets now use a pool.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,9 +9,12 @@ public class GameController : MonoBehaviour
 	private float nextSpawnTime;
 	private float spawnFrequency; //In seconds.
 
+	private GameObject[] enemyBulletPool;
+
 	// Use this for initialization
 	void Start ()
 	{
+		enemyBulletPool = new GameObject[GameSettings.ENEMY_BULLET_POOL_SIZE];
 		nextSpawnTime = Time.time;
 		spawnFrequency = 1;
 	}
@@ -26,10 +29,45 @@ public class GameController : MonoBehaviour
 	{
 		if (Time.time > nextSpawnTime)
 		{
-			GameObject newEnemy = (GameObject)Instantiate(enemyShot, spawnPostion, Quaternion.identity);
-			newEnemy.GetComponent<EnemyShot>().Direction = Vector3.down;
+			GameObject newEnemy = getBulletFromPool();
+			if (newEnemy != null)
+			{
+				newEnemy.GetComponent<EnemyShot>().Reset(spawnPostion);
+				newEnemy.GetComponent<EnemyShot>().Direction = Vector3.down;
+			}
 
 			nextSpawnTime = Time.time + spawnFrequency;
 		}
+	}
+
+	GameObject getBulletFromPool()
+	{
+		bool fired = false;
+		for (int bulletID = 0; bulletID < enemyBulletPool.Length; bulletID++)
+		{
+			if (enemyBulletPool[bulletID] != null)
+			{
+				if (enemyBulletPool[bulletID].activeSelf == false)
+				{
+					return enemyBulletPool[bulletID];
+				}
+			}
+		}
+		
+		//No free bullet
+		if (!fired)
+		{
+			for (int bulletID = 0; bulletID < enemyBulletPool.Length; bulletID++)
+			{
+				if (enemyBulletPool[bulletID] == null)
+				{
+					enemyBulletPool[bulletID] = (GameObject)Instantiate(enemyShot, transform.position, Quaternion.identity);
+					return enemyBulletPool[bulletID];
+				}
+			}
+		}
+
+		Debug.LogWarning("Enemy bullet pool full!");
+		return null;
 	}
 }
