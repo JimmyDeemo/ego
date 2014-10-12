2aa888c72a54655e1108683e01271dcec17049f0
James Deeman <james.deeman@gmail.com>
2014-10-12 21:55:31 +0100

    Increased the pool sizes. Added fire rates.

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class GameSettings
 {
 	#region Player Settings
-	public static int PLAYER_BULLET_POOL_SIZE = 10;
+	public static float PLAYER_RATE_OF_FIRE = 0.1f; //In seconds.
+	public static int PLAYER_BULLET_POOL_SIZE = 15;
 	#endregion
 
 	#region Enemy Settings
-	public static int ENEMY_BULLET_POOL_SIZE = 100; 
+	public static int ENEMY_BULLET_POOL_SIZE = 100;
+	public static float ENEMY_RATE_OF_SPAWN = 0.2f; //In seconds.
 	#endregion
 }
