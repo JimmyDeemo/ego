f10bf9e4207db309ba40659e3a828c622bc6e607
James Deeman <james.deeman@gmail.com>
2014-10-12 20:30:37 +0100

    Added class to use as a settings controller.

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings
+{
+	#region Player Settings
+	public static int PLAYER_BULLET_POOL_SIZE = 10;
+	#endregion
+
+	#region Enemy Settings
+	public static int ENEMY_BULLET_POOL_SIZE = 100; 
+	#endregion
+}
