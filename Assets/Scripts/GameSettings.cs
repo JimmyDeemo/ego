2ca10d008db40fac03c2d52fd16cdf62d359e5e4
James Deeman <james.deeman@gmail.com>
2014-10-14 20:32:08 +0100

    Tweaked the setting to spawn less enemies and increase the player bullet pool size.

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -5,7 +5,7 @@ public class GameSettings
 {
 	#region Player Settings
 	public static float PLAYER_RATE_OF_FIRE = 0.1f; //In seconds.
-	public static int PLAYER_BULLET_POOL_SIZE = 15;
+	public static int PLAYER_BULLET_POOL_SIZE = 20;
 	public static float PLAYER_SCALE_FACTOR = 1.005f;
 	#endregion
 
@@ -13,8 +13,8 @@ public class GameSettings
 	public static int SHOTGUN_SPREAD = 2;
 
 	public static int ENEMY_BULLET_POOL_SIZE = 100;
-	public static float ENEMY_RATE_OF_SPAWN_MIN = 0.4f; //In seconds.
-	public static float ENEMY_RATE_OF_SPAWN_MAX = 0.8f; //In seconds.
+	public static float ENEMY_RATE_OF_SPAWN_MIN = 0.6f; //In seconds.
+	public static float ENEMY_RATE_OF_SPAWN_MAX = 1.2f; //In seconds.
 	public static float ENEMY_SPEED_MIN = 3.0f;
 	public static float ENEMY_SPEED_MAX = 5.0f;
 	#endregion
