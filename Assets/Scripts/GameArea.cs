1a947226c9bf1ae399dd9f2178642f0f97b3db75
James Deeman <jamesd@mazooma.co.uk>
2014-10-21 13:49:47 +0100

    Added comments to these two files.

--- Assets/Scripts/GameArea.cs
+++ Assets/Scripts/GameArea.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Simple Class script for the Game Area object.
+/// The primary role of this object is to detect when game objects have
+/// left the playable area.
+/// </summary>
 public class GameArea : MonoBehaviour
 {
-	void OnTriggerExit2D(Collider2D coll)
+    /// <summary>
+    /// Trigger function called by Unity when the game area detects an object
+    /// has exited it's bounds.
+    /// </summary>
+    /// <param name="coll">Collision object of the 2D game object that has left the area.</param>
+	private void OnTriggerExit2D(Collider2D coll)
 	{
-		//Identify those that need destroying or 
+		//Identify those that need destroying.
 		if (coll.gameObject.tag == "PlayerBullet" || coll.gameObject.tag == "EnemyBullet")
 		{
 			if (coll.gameObject.tag == "PlayerBullet" && coll.gameObject.GetComponent<Bullet>().isSuper)
