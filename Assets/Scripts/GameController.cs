579c15b0b30950b7ac304b96f8f6892b089d5e42
James Deeman <james.deeman@gmail.com>
2015-02-14 22:28:38 +0000

    Cleaned up the GameController class.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
+
+#region Public member variables.
 	public GameObject enemyShot;
 	public Vector2 spawnPostionMin;
 	public Vector2 spawnPostionMax;
 
 	public GameObject playerRef;
-	private Transform playerTransform;
-	private Player playerScript;
-
 	public GameObject howToPlayRef;
     public GameObject logoRef;
 	public GameObject scoreRef;
@@ -20,17 +19,24 @@ public class GameController : MonoBehaviour
 
 	public Vector3 shieldMeterFullSize;
 	public Vector3 shieldMeterDefaultPosition;
+#endregion
 
+#region Public member variables.
 	private int score;
 	private int highScore;
+
+	private Transform playerTransform;
+	private Player playerScript;
 
 	private float nextSpawnTime;
 
 	private GameObject[] enemyBulletPool;
+#endregion
 
-
-	// Use this for initialization
-	void Start ()
+	/// <summary>
+	/// Initialisation function used by Unity.
+	/// </summary>
+	private void Start ()
 	{
 #if UNITY_ANDROID
 		howToPlayRef.guiText.text = "Touch (and hold) screen to start.";
@@ -53,7 +59,10 @@ public class GameController : MonoBehaviour
 		score = -1;
 	}
 
-    void ResetGame()
+	/// <summary>
+	/// Reset all elements in the game. Genarally used when we want to begin a new game.
+	/// </summary>
+   	private void ResetGame()
     {
 		score = 0;
         playerRef.GetComponent<Player>().Reset();
@@ -71,8 +80,10 @@ public class GameController : MonoBehaviour
 		shieldMeterRef.transform.position = shieldMeterDefaultPosition;
     }
 	
-	// Update is called once per frame
-	void Update ()
+	/// <summary>
+	/// Update function used by Unity.
+	/// </summary>
+	private void Update ()
 	{
         //Quit?
 		if ( Input.GetKeyDown(KeyCode.Escape) )
@@ -81,10 +92,12 @@ public class GameController : MonoBehaviour
 			return;
 		}
 
+		//Is player alive?
         if (playerRef.activeSelf)
         {
 			SetOverlayVisibility(false);
 
+			//Shield animation and status.
 			if (!playerScript.ShieldActive)
 			{
 				float ratio = (Time.time - playerScript.ShieldDeactivateTime) / (playerScript.ShieldReactivateTime - playerScript.ShieldDeactivateTime);
@@ -107,13 +120,14 @@ public class GameController : MonoBehaviour
         }
 		else
         {
+			//Show the start screen.
 			highScore = Mathf.Max( score, highScore );
 			SetOverlayVisibility(true);
 
 #if UNITY_ANDROID
 			if ( Input.GetMouseButtonDown( 0 ) )
 #else
-				if ( Input.GetKeyDown(KeyCode.R) )
+			if ( Input.GetKeyDown(KeyCode.R) )
 #endif
             {
                 ResetGame();
@@ -122,17 +136,19 @@ public class GameController : MonoBehaviour
 	}
 
 	/// <summary>
-	/// TODO
+	/// Sets whether or not we can see the start screen assets or not. Shows/Hides the game logo as well as the last game's score and the current high score.
 	/// </summary>
 	/// <param name="isVisable">If set to <c>true</c> is visable.</param>
 	private void  SetOverlayVisibility( bool isVisable )
 	{
+		//Start screen asssets and game UI should be mutually exclusive.
 		logoRef.renderer.enabled = isVisable;
 		howToPlayRef.SetActive(isVisable);
 
 		scoreRef.SetActive(!isVisable);
 		shieldMeterRef.SetActive(!isVisable);
 
+		//Only show the rest of the detils we we have them.
 		if (score != -1)
 		{
 			prevScoreRef.GetComponent<GUIText>().text = GameSettings.PREVIOUS_SCORE_TEXT + score.ToString();
@@ -176,10 +192,9 @@ public class GameController : MonoBehaviour
 
 	/// <summary>
 	/// Spawns a cluster of enemies dependant on type.
-	/// 
-	/// </summary>
+	/// 	/// </summary>
 	/// <param name="typeID"></param>
-	void SpawnCluster( string typeID )
+	private void SpawnCluster( string typeID )
 	{
 		GameObject[] bulletsToSpawn;
 		Vector2 spawnCenter = new Vector2( Random.Range(spawnPostionMin.x,spawnPostionMax.x), Random.Range(spawnPostionMin.y, spawnPostionMax.y));
@@ -195,36 +210,35 @@ public class GameController : MonoBehaviour
 				{
 					if (spawn != null)
 					{
-						fireDirection = new Vector2( Random.Range(playerTransform.position.x - GameSettings.SHOTGUN_SPREAD, playerTransform.position.x + GameSettings.SHOTGUN_SPREAD), playerTransform.position.y );
+						//From center point, targetted slightly to the left or right of the player.
+						fireDirection = new Vector2( Random.Range(playerTransform.position.x - GameSettings.SHOTGUN_SPREAD, playerTransform.position.x + GameSettings.SHOTGUN_SPREAD),
+						                             playerTransform.position.y
+						                           );
 						fireDirection = fireDirection - spawnCenter;
 						fireDirection.Normalize();
-						//From center point, targetted slightly to the left or right of the player.
-						spawn.GetComponent<EnemyShot>().Reset( spawnCenter,
-						                                      fireDirection,
-						                                      Random.Range(GameSettings.ENEMY_SPEED_MIN, GameSettings.ENEMY_SPEED_MAX)
-						            						);
+
+						spawn.GetComponent<EnemyShot>().Reset( spawnCenter, fireDirection, Random.Range(GameSettings.ENEMY_SPEED_MIN, GameSettings.ENEMY_SPEED_MAX) );
 					}
 				}
 				break;
-
+			
+			//Shots fly from center point out in an evenly distributed arc.
 			case "pulse":
 				int numShotsInPulse = 9;
 				bulletsToSpawn = RequestBulletsFromPool(numShotsInPulse);
 				fireDirection = -Vector2.up;
+
+				//Calculate the angular separation of each bullet.
 				float degreesPerShot = GameSettings.PULSE_SPREAD / (numShotsInPulse - 1);
 				Quaternion tempRotation = Quaternion.AngleAxis( -GameSettings.PULSE_SPREAD * 0.5f, Vector3.forward ); //Creates a rotation around the vector that is facing the camera.
-				fireDirection = tempRotation * fireDirection; //Star point of the arc shaped pulse.
+				fireDirection = tempRotation * fireDirection; //Start point of the arc shaped pulse.
 				tempRotation = Quaternion.AngleAxis( degreesPerShot, Vector3.forward ); //Rotation to alter each time.
 
 				foreach (var spawn in bulletsToSpawn)
 				{
 					if (spawn != null)
 					{
-						//From center point, targetted slightly to the left or right of the player.
-						spawn.GetComponent<EnemyShot>().Reset( spawnCenter,
-						                                      fireDirection,
-						                                      GameSettings.ENEMY_SPEED_MIN
-						                                      );
+						spawn.GetComponent<EnemyShot>().Reset( spawnCenter, fireDirection, GameSettings.ENEMY_SPEED_MIN );
 					}
 
 					fireDirection = tempRotation * fireDirection;
@@ -236,7 +250,12 @@ public class GameController : MonoBehaviour
 		}
 	}
 
-	GameObject[] RequestBulletsFromPool(int numberOfBullets)
+	/// <summary>
+	/// Function to find a number of requested inactive bullets from the pool and return them. Pools are used so that we don't create too many object; reusing older ones is more efficient.
+	/// </summary>
+	/// <returns>The bullet objects requested.</returns>
+	/// <param name="numberOfBullets">Number of bullets needed.</param>
+	private GameObject[] RequestBulletsFromPool(int numberOfBullets)
 	{
 		//Lets clamp so we know that we always need at least one.
 		if (numberOfBullets <= 0)
@@ -251,7 +270,7 @@ public class GameController : MonoBehaviour
 		{
 			if (enemyBulletPool[bulletID] != null)
 			{
-				if (enemyBulletPool[bulletID].activeSelf == false)
+				if (enemyBulletPool[bulletID].activeSelf == false) //Inactive bullets are available for reuse.
 				{
 					bulletAllocation[numberAllocated++] = enemyBulletPool[bulletID];
 					if (numberAllocated == numberOfBullets)
@@ -287,7 +306,10 @@ public class GameController : MonoBehaviour
 		return bulletAllocation;
 	}
 
-	void ScoreHit()
+	/// <summary>
+	/// Delagate function called from the player object when it gets a hit. Function is allocated to player on initialisation.
+	/// </summary>
+	private void ScoreHit()
 	{
 		//Check here because other wise player can score with bullets that are loose after death.
 		if (playerRef.activeSelf)
