bcedfdfde94d9bea927c88d3d62d5485a63269b1
James Deeman <james.deeman@gmail.com>
2016-03-07 23:06:46 +0000

    Enemies no inherit from a the base class Enemy which contains some common methods. Pulse and Shogun enemies have their own firing coroutine. These could be extended to allow for multiple burst or time base firing. Spray enemy is just a skeleton class for now.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
+	public string ExitAnimatorParameterName = "Exit";
+
+	protected Animator m_Animator;
+
+	private int m_ExitHash;
+
+	public abstract void CommenceFiring();
+
+	protected void FiringEnded()
+	{
+		m_Animator.SetBool(m_ExitHash, true);
+	}
+
+	public virtual void Awake()
+	{
+		m_Animator = GetComponent<Animator>();
+
+		m_ExitHash = Animator.StringToHash(ExitAnimatorParameterName);
+	}
+
 }
--- Assets/Scripts/ShotgunEnemy.cs
+++ Assets/Scripts/ShotgunEnemy.cs
@@ -3,4 +3,64 @@ using System.Collections;
 
 public class ShotgunEnemy : Enemy
 {
+	public int BulletsInBurst = 10;
+	public float ShotgunSpread = 3.0f;
+	public float BulletSpeedMin = 2.0f;
+	public float BulletSpeedMax = 3.0f;
+
+	private Coroutine m_Coroutine;
+	private Transform m_PlayerTransform;
+
+	public override void CommenceFiring()
+	{
+		m_Coroutine = StartCoroutine(FireCoroutine());
+	}
+
+	private IEnumerator FireCoroutine()
+	{
+		FireBurst();
+
+		yield return null;
+
+		FiringEnded();
+	}
+
+	private void FireBurst()
+	{
+		GameObject[] bulletsToSpawn;
+		Vector2 spawnCenter = transform.position;
+		Vector2 fireDirection;
+
+		bulletsToSpawn = GameController.Instance.RequestBulletsFromPool(BulletsInBurst);
+
+		foreach (var spawn in bulletsToSpawn)
+		{
+			if (spawn != null)
+			{
+				//From center point, targetted slightly to the left or right of the player.
+				fireDirection = new Vector2(Random.Range(m_PlayerTransform.position.x - ShotgunSpread, m_PlayerTransform.position.x + ShotgunSpread),
+											 m_PlayerTransform.position.y
+										   );
+				fireDirection = fireDirection - spawnCenter;
+				fireDirection.Normalize();
+
+				spawn.GetComponent<EnemyShot>().Reset(spawnCenter, fireDirection, Random.Range(BulletSpeedMin, BulletSpeedMax));
+			}
+		}
+	}
+
+	public override void Awake()
+	{
+		base.Awake();
+
+		m_PlayerTransform = GameController.Instance.Player.transform;
+	}
+
+	public void OnDestroy()
+	{
+		if (m_Coroutine != null)
+		{
+			StopCoroutine(m_Coroutine);
+		}
+	}
 }
